33850978258f9cf5e1bc63478fe6a4b3fafd6e73
Luca Laureno <luca.laureno@gmail.com>
2026-01-02 11:43:03 -0500

    implemented permitted login hours logic

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,5 +1,6 @@
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+from datetime import datetime
 import re
 import json
 
@@ -9,6 +10,9 @@ def load_config():
 
 config = load_config()
 permitted_ips = config["permitted_ips"]
+permitted_login_hours = config["allowed_login_hours"]
+start_hour = permitted_login_hours["start"]
+end_hour = permitted_login_hours["end"]
 
 def pick_file():
     Tk().withdraw()
@@ -25,14 +29,17 @@ def analyze_log(filename):
     print(f"Analyzing: {filename}")
     with open(filename, "r") as f:
         for line in f:
-            match = re.search(r"\b\d{1,3}(?:\.\d{1,3}){3}\b", line)
-            if match:
-                ip = match.group()
+            ip_match = re.search(r"\b\d{1,3}(?:\.\d{1,3}){3}\b", line)
+            if ip_match:
+                ip = ip_match.group()
                 if ip not in permitted_ips:
                     print("POTENTIAL SUSPICIOUS IP >", ip)
-            '''Example: detect failed SSH login attempts
-            if "Failed password" in line:
-                print("FAILED LOGIN >", line.strip())'''
+            time_match = re.search(r"\d{4}-\d{2}-\d{2} (\d{2}):\d{2}:\d{2}", line)
+            if not time_match:
+                return
+            hour = int(time_match.group(1))
+            if hour < start_hour or hour >= end_hour:
+                print("POTENTIAL UNUSUAL LOGIN TIME >", time_match.group(0))
             
 def main():
     log_file = pick_file()
